c0298328aa0cbc7afd20e5dbe463343821beeac9
Matthias Eichner <thegreatmewel@gmail.com>
2017-11-14 00:16:51 +0100

    validate level before start

--- core/src/de/mewel/pixellogic/PixelLogicGame.java
+++ core/src/de/mewel/pixellogic/PixelLogicGame.java
@@ -1,11 +1,13 @@
 package de.mewel.pixellogic;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 import de.mewel.pixellogic.model.PixelLogicLevel;
 import de.mewel.pixellogic.model.PixelLogicLevelCollection;
 import de.mewel.pixellogic.util.PixelLogicLevelLoader;
 import de.mewel.pixellogic.util.PixelLogicUtil;
+import de.mewel.pixellogic.util.PixelLogicValidator;
 
 public class PixelLogicGame extends Game {
 
@@ -16,6 +18,9 @@ public class PixelLogicGame extends Game {
         PixelLogicLevelCollection levelCollection = levelManager.getCollection("rpgitems");
 
         PixelLogicLevel level = PixelLogicLevelLoader.load(levelCollection, 0);
+
+        boolean valid = new PixelLogicValidator().validate(level.getLevelData());
+        Gdx.app.log("validate level", "level is " + (valid ? "valid" : "invalid"));
 
         PixelLogicGameScreen screen = new PixelLogicGameScreen();
         screen.loadLevel(level);
