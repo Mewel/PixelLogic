0f0a75ce2a835b246b8f961977eabfef6b1f4479
Matthias Eichner <thegreatmewel@gmail.com>
2017-11-25 23:12:03 +0100

    start resolution managing

--- core/src/de/mewel/pixellogic/gui/PixelLogicGUIResolutionManager.java
+++ core/src/de/mewel/pixellogic/gui/PixelLogicGUIResolutionManager.java
@@ -1,15 +1,20 @@
 package de.mewel.pixellogic.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PixelLogicGUIResolutionManager {
 
     private static PixelLogicGUIResolutionManager INSTANCE;
 
-    private PixelLogicGUIResolutionManager() {
+    private List<PixelLogicGUIResolution> resolutions;
 
+    private PixelLogicGUIResolutionManager() {
+        this.resolutions = new ArrayList<PixelLogicGUIResolution>();
     }
 
     public static PixelLogicGUIResolutionManager instance() {
-        if(INSTANCE == null) {
+        if (INSTANCE == null) {
             INSTANCE = new PixelLogicGUIResolutionManager();
             INSTANCE.init();
         }
@@ -17,7 +22,33 @@ public class PixelLogicGUIResolutionManager {
     }
 
     private void init() {
+        int fontScale = 2;
+        // 320
+        resolutions.add(new PixelLogicGUIResolution(320, 21, 1, 8 * fontScale));
+        // 360
+        resolutions.add(new PixelLogicGUIResolution(360, 24, 1, 8 * fontScale));
+        // 480
+        resolutions.add(new PixelLogicGUIResolution(480, 32, 2, 16 * fontScale));
+        // 540
+        resolutions.add(new PixelLogicGUIResolution(540, 36, 2, 16 * fontScale));
+        // 640
+        resolutions.add(new PixelLogicGUIResolution(640, 42, 3, 16 * fontScale));
+        // 720
+        resolutions.add(new PixelLogicGUIResolution(720, 48, 3, 24 * fontScale));
+        // 960
+        resolutions.add(new PixelLogicGUIResolution(960, 64, 4, 32 * fontScale));
+        // 1080
+        resolutions.add(new PixelLogicGUIResolution(1080, 72, 4, 32 * fontScale));
+        // 1440
+        resolutions.add(new PixelLogicGUIResolution(1440, 96, 6, 48 * fontScale));
+    }
 
+    public PixelLogicGUIResolution get(int screenWidth, int screenHeight) {
+        int min = Math.min(screenWidth, screenHeight);
+        PixelLogicGUIResolution resolution;
+        for (PixelLogicGUIResolution rs : resolutions) {
+
+        }
     }
 
 }
