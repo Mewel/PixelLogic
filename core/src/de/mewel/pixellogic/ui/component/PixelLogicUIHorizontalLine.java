b109817eaf6b3b0dd1d1ea9c2ef664c52e46ab4f
Mewel <thegreatmewel@gmail.com>
2018-03-08 21:55:02 +0100

    try to add horizontal line

--- core/src/de/mewel/pixellogic/ui/component/PixelLogicUIHorizontalLine.java
+++ core/src/de/mewel/pixellogic/ui/component/PixelLogicUIHorizontalLine.java
@@ -1,6 +1,8 @@
 package de.mewel.pixellogic.ui.component;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import de.mewel.pixellogic.asset.PixelLogicAssets;
 import de.mewel.pixellogic.event.PixelLogicEventManager;
@@ -8,14 +10,42 @@ import de.mewel.pixellogic.ui.PixelLogicUIActor;
 
 public class PixelLogicUIHorizontalLine extends PixelLogicUIActor {
 
+    private ShapeRenderer renderer;
+
     public PixelLogicUIHorizontalLine(PixelLogicAssets assets, PixelLogicEventManager eventManager, int height) {
         super(assets, eventManager);
         this.setHeight(height);
+        this.renderer = new ShapeRenderer();
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
+        batch.end();
+
+        this.renderer.setProjectionMatrix(batch.getProjectionMatrix());
+        this.renderer.setTransformMatrix(batch.getTransformMatrix());
+        this.renderer.translate(getX(), getY(), 0);
+        this.renderer.begin(ShapeRenderer.ShapeType.Line);
+        this.renderer.box(0f, 0f, 0, getWidth(), getHeight(), 0f);
+        this.renderer.end();
+
+        batch.begin();
+
+        /*batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
+
+        Integer gridY = GRID.get(level.getRows());
+        if (gridY != null) {
+            for (int y = gridY; y < level.getRows(); y += gridY) {
+                batch.draw(gridTexture, getX(), getY() + (combined * y) - spaceSize, xWidth, spaceSize);
+            }
+        }
+        Integer gridX = GRID.get(level.getColumns());
+        if (gridX != null) {
+            for (int x = gridX; x < level.getColumns(); x += gridX) {
+                batch.draw(gridTexture, getX() + (combined * x) - spaceSize, getY(), spaceSize, yWidth);
+            }
+        }*/
     }
 
 }
--- core/src/de/mewel/pixellogic/ui/screen/PixelLogicUITimeTrialScreen.java
+++ core/src/de/mewel/pixellogic/ui/screen/PixelLogicUITimeTrialScreen.java
@@ -1,12 +1,14 @@
 package de.mewel.pixellogic.ui.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 
 import de.mewel.pixellogic.asset.PixelLogicAssets;
 import de.mewel.pixellogic.event.PixelLogicEventManager;
@@ -17,96 +19,56 @@ import static de.mewel.pixellogic.ui.PixelLogicUIConstants.TEXT_COLOR;
 
 public class PixelLogicUITimeTrialScreen extends PixelLogicUIScreen {
 
-    private Table table;
+    private VerticalGroup root;
 
-    private Label descriptionLabel, normalLabel, hardLabel;
-
-    private PixelLogicUIButton playNormalButton, playHardButton;
+    private AssetStore assetStore;
 
     public PixelLogicUITimeTrialScreen(PixelLogicAssets assets, PixelLogicEventManager eventManager) {
         super(assets, eventManager);
+        this.assetStore = new AssetStore(getAssets(), getEventManager());
         buildGUI();
     }
 
     protected void buildGUI() {
-        BitmapFont normalFont = getAssets().getGameFont(PixelLogicUIUtil.getTextHeight());
-        Label.LabelStyle normalStyle = new Label.LabelStyle(normalFont, TEXT_COLOR);
-
-        BitmapFont smallFont = getAssets().getGameFont(PixelLogicUIUtil.getTextHeight() / 2);
-        Label.LabelStyle smallStyle = new Label.LabelStyle(smallFont, TEXT_COLOR);
-
-        int screenWidth = Gdx.graphics.getWidth();
-        int padding = screenWidth / 18;
-
         // labels
-        this.descriptionLabel = new Label("Play infinite auto generated levels against the " +
-                "clock and try to beat your highscore.", normalStyle);
-        this.descriptionLabel.setWrap(true);
+        Label descriptionLabel = new Label("Play infinite auto generated levels against the " +
+                "clock and try to beat your highscore.", assetStore.getNormalLabelStyle());
+        descriptionLabel.setWrap(true);
 
-        this.normalLabel = new Label("NORMAL MODE", normalStyle);
-        this.hardLabel = new Label("HARD MODE", normalStyle);
+        // combine to VerticalGroup
+        this.root = new VerticalGroup();
+        this.root.setFillParent(true);
+        this.root.center();
+        this.root.bottom();
+        this.root.reverse();
+        this.root.pad(getPadding());
+        this.root.space(getPadding() * 2);
 
-        // buttons
-        this.playNormalButton = new PixelLogicUIButton(getAssets(), getEventManager(), "PLAY");
-        this.playNormalButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-
-            }
-        });
-
-        this.playHardButton = new PixelLogicUIButton(getAssets(), getEventManager(), "PLAY");
-        this.playHardButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-
-            }
-        });
-        int buttonWidth = getButtonWidth();
-        int buttonHeight = getButtonHeight();
-        this.playNormalButton.setSize(buttonWidth, buttonHeight);
-        this.playHardButton.setSize(buttonWidth, buttonHeight);
-
-        // highscore
-        Label normalHighscoreLabel = new Label("highscore", smallStyle);
-
-        // combine to table
-        this.table = new Table();
-        this.table.setFillParent(true);
-        this.table.setDebug(true);
-
-        this.table.add(this.playHardButton).padBottom(padding).expand().bottom();
-        this.table.row();
-        this.table.add(this.hardLabel).padBottom(padding);
+        Container<Label> labelContainer = new Container<Label>(descriptionLabel);
+        labelContainer.width(getComponentWidth());
+        this.root.addActor(labelContainer);
+        this.root.addActor(new Mode("NORMAL MODE", assetStore));
+        this.root.addActor(new Mode("HARD MODE", assetStore));
 
         // ShapeRenderer
 
-        this.table.row();
-        this.table.add(normalHighscoreLabel).padBottom(padding / 2);
-        this.table.row();
-        this.table.add(this.playNormalButton).padBottom(padding / 2);
-        this.table.row();
-        this.table.add(this.normalLabel).padBottom(padding);
+        //this.root.add(normalHighscoreLabel).padBottom(padding / 2).left();
+        //this.root.add(normalTimeLabel).padBottom(padding / 2).right();
 
-        this.table.row();
-        this.table.add(this.descriptionLabel).growX();
+        getStage().addActor(this.root);
+        this.root.layout();
+    }
 
-        this.table.pad(padding);
-        getStage().addActor(this.table);
+    public static int getPadding() {
+        return Gdx.graphics.getWidth() / 18;
+    }
+
+    public static int getComponentWidth() {
+        return Gdx.graphics.getWidth() - 2 * getPadding();
     }
 
     protected void cleanGUI() {
-        getStage().getRoot().removeActor(this.table);
+        getStage().getRoot().removeActor(this.root);
     }
 
     @Override
@@ -141,12 +103,94 @@ public class PixelLogicUITimeTrialScreen extends PixelLogicUIScreen {
         this.buildGUI();
     }
 
-    private int getButtonHeight() {
+    private static int getButtonHeight() {
         return PixelLogicUIUtil.getBaseHeight();
     }
 
-    private int getButtonWidth() {
+    private static int getButtonWidth() {
         return getButtonHeight() * 4;
+    }
+
+    private static class Mode extends Container<VerticalGroup> {
+
+        public Mode(String name, AssetStore assetStore) {
+            super(new VerticalGroup());
+
+            getActor().setFillParent(true);
+            getActor().center();
+            getActor().bottom();
+            getActor().reverse();
+            getActor().space(10);
+
+            Label normalHighscoreLabel = new Label("highscore", assetStore.getSmallLabelStyle());
+            Label normalTimeLabel = new Label("time", assetStore.getSmallLabelStyle());
+            Label header = new Label(name, assetStore.getNormalLabelStyle());
+
+            PixelLogicUIButton button = new PixelLogicUIButton(assetStore.getAssets(), assetStore.getEventManager(), "PLAY");
+            button.addListener(new InputListener() {
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                    return true;
+                }
+
+                @Override
+                public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+
+                }
+            });
+            button.setSize(getButtonWidth(), getButtonHeight());
+            getActor().addActor(header);
+            getActor().addActor(button);
+
+            Table highscoreTable = new Table();
+            Container<Table> highscoreContainer = new Container<Table>(highscoreTable);
+            highscoreContainer.width(getComponentWidth());
+            highscoreTable.setFillParent(true);
+            //highscoreTable.setDebug(true);
+            highscoreTable.add(normalHighscoreLabel).growX().left();
+            highscoreTable.add(normalTimeLabel).right();
+
+            getActor().addActor(highscoreContainer);
+        }
+
+    }
+
+
+    private static class AssetStore {
+
+        private PixelLogicAssets assets;
+
+        private PixelLogicEventManager eventManager;
+
+        private Label.LabelStyle normalLabelStyle, smallLabelStyle;
+
+        private AssetStore(PixelLogicAssets assets, PixelLogicEventManager eventManager) {
+            this.assets = assets;
+            this.eventManager = eventManager;
+
+            BitmapFont normalFont = getAssets().getGameFont(PixelLogicUIUtil.getTextHeight());
+            this.normalLabelStyle = new Label.LabelStyle(normalFont, TEXT_COLOR);
+
+            BitmapFont smallFont = getAssets().getGameFont(PixelLogicUIUtil.getTextHeight() / 2);
+            this.smallLabelStyle = new Label.LabelStyle(smallFont, TEXT_COLOR);
+        }
+
+        public PixelLogicAssets getAssets() {
+            return assets;
+        }
+
+        public PixelLogicEventManager getEventManager() {
+            return eventManager;
+        }
+
+        public Label.LabelStyle getNormalLabelStyle() {
+            return normalLabelStyle;
+        }
+
+        public Label.LabelStyle getSmallLabelStyle() {
+            return smallLabelStyle;
+        }
+
     }
 
 }
