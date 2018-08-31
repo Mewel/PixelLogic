eafd8e0cf7402cb12cb70e1b06fcf4f3a5313a6c
Matthias Eichner <thegreatmewel@gmail.com>
2018-08-31 03:48:11 +0200

    improve 100 characters levels

--- core/src/de/mewel/pixellogic/CharactersLevelTest.java
+++ core/src/de/mewel/pixellogic/CharactersLevelTest.java
@@ -37,7 +37,7 @@ public class CharactersLevelTest extends Game implements PixelLogicGlobal {
         final PixelLogicCharactersMode mode = new PixelLogicCharactersMode();
         mode.setup(this);
         mode.reset();
-        PixelLogicLevel level = mode.findLevel("Sideshow Bob");
+        PixelLogicLevel level = mode.findLevel("Dhalsim");
         mode.runLevel(level);
 
         PixelLogicUIPageProperties pageProperties = new PixelLogicUIPageProperties();
